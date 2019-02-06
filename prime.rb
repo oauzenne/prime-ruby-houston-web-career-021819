39cd5d5cfc40c9a70fbd6101b83770db78a66e31
Olivia Auzenne <oauzenne@gmail.com>
2019-02-06 02:21:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,12 @@
-def prime?(number)
-  number.include?(prime)
+def prime?(n)
+  is_prime = true
+  if n <= 1
+    is_prime = false
+  end
+  for i in 2...n
+    if n % i == 0
+      is_prime = false
+    end
+  end
+  is_prime
 end
